1184070b6aa0753456beb5c2ac2dd97f62cfd665
wgabriel14 <16-10973@usb.ve>
2022-04-08 14:45:50 -0400

    Aplicando herencia en JAVA

--- Java/Car.java
+++ Java/Car.java
@@ -2,13 +2,15 @@ class Car {
     Integer id;
     String license;
     Account driver;
-    Integer passenger;
+    Integer passegenger;
 
     public Car(String license, Account driver){
         this.license = license;
         this.driver = driver;
     }
+
     void printDataCar() {
-        System.out.println("License: " + license + "Name Driver: " + driver);
+        System.out.println("License: " + license + " Name Driver: " + driver.name);
     }
+
 }
