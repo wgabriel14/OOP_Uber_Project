096b2e60da951336d647903e58ec9c05726d4ced
wgabriel14 <16-10973@usb.ve>
2022-04-08 15:06:28 -0400

    Aplicando herencia en Javascript creando las instancias UberX UberPool UberBlack UberVan

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
-var car = new Car("VZLA123", new Account("Williams Reyes", "CI123"));
+var car = new Car("VZLA123", new Account("Williams Reyes", "CI123"))
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new UberX("USB1234", new Account("Elon Musk", "CI1234"), "Chevrolet", "Spark")
+uberX.passenger = 4;
+uberX.printDataCar();
