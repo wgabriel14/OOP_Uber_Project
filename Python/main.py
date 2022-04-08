5b1932fc5ce9d3de79d1d06ad9b6eb3c8f83c464
wgabriel14 <16-10973@usb.ve>
2022-04-08 00:55:41 -0400

    Se crearon dos objetos de la clase Car en python

--- Python/main.py
+++ Python/main.py
@@ -1,3 +1,18 @@
+"""
+En python debemos empezar importando el elemento
+"""
+from car import Car
+
 if __name__ == "__main__":
     print("Hola mundo!")
+    car = Car()
+    car.license = "VZLA123"
+    car.driver = "Williams Reyes"
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "USB1234"
+    car2.driver = "Elon Musk"
+    print(vars(car2))
+
     
